c37d6c73e7fa6613981807b088186406d5ef045f
kabilan <kabilan@example.com>
2026-07-20 19:17:17 +0530

    spiralmatrix

---
+++ spiralMatrix.java
@@ -0,0 +1,39 @@
+public class spiralMatrix {
+    public static void main (String args[]){
+        int arr[][] = {{1,2,3,4},
+                       {5,6,7,8},
+                       {9,10,11,12},
+                       {13,14,15,16}};
+        spiralmatrix(arr);
+    }
+    public static void spiralmatrix(int [][]arr){
+        int n = arr.length;
+        int m = arr[0].length;
+        int cmin = 0 , cmax = arr[0].length-1;
+        int rmin = 0 , rmax = arr[0].length-1;
+        int count = 0;
+        while (count < n*m) {
+            for (int col = cmin ; col<=cmax && count <n*m ; col++){
+                System.out.print(arr[rmin][col]+ " ");
+                count++;
+            }
+            rmin++;
+            for (int row=rmin ; row<=rmax && count<n*m ; row++){
+                System.out.print(arr[row][cmax] + " ");
+                count++;
+            }
+            cmax--;
+           for(int col = cmax ; col>=cmin && count < n*m ; col --){
+                System.out.print(arr[rmax][col]+ " ");
+                count++;
+            }
+            rmax--;
+           for (int row = rmax ; row >= rmin && count <n*m ; row --){
+                System.out.print(arr[row][cmin]+ " ");
+                count++;
+            }
+            cmin++;
+        }
+    }
+    
+}
